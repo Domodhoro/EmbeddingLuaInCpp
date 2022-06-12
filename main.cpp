00845a462a1bbdbef4035f2bd299c00e9b36b62a
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-06-12 14:33:14 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,12 +13,12 @@ int main() {
         cout << "Digite o caminho do script lua: ";
         cin >> scriptPath;
 
-        auto luaScript = std::make_unique<LuaScript>(scriptPath.c_str());
+        auto luaScript = make_unique<LuaScript>(scriptPath.c_str());
 
         cin.ignore();
         cin.get();
         return 0;
-    } catch(const std::exception& e) {
+    } catch(const exception& e) {
         cerr << e.what();
         cin.ignore();
         cin.get();
