054009d9fe962b720fc9e55f7abdddb69c2bde0d
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-07-18 13:23:34 -0300

    Add files via upload

---
+++ cocktailSort.lua
@@ -0,0 +1,42 @@
+cocktailSort = function(myItems, size)
+    local swapped
+    repeat
+    swapped = false
+
+    for i = 1, size - 1 do
+        if myItems[i] > myItems[i + 1] then
+            myItems[i], myItems[i + 1] = myItems[i + 1], myItems[i]
+
+            swapped = true
+        end
+    end
+
+    if swapped == false then
+        break
+	end
+
+    for i = size - 1, 1, -1 do
+        if myItems[i] > myItems[i + 1] then
+            myItems[i], myItems[i + 1] = myItems[i + 1] , myItems[i]
+
+            swapped = true
+        end
+    end
+
+    until swapped == false
+end
+
+main = function()
+    local myItems = {
+        2.54, 5.85, 4.18, 0.54, 4.36, 2.33,
+        6.36, 3.32, 7.65, 4.56, 9.56, 1.24
+    }
+
+    local size = #myItems
+
+    cocktailSort(myItems, size)
+
+    for i = 1, size do
+        print(myItems[i])
+    end
+end
